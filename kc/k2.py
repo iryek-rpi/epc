555dcb1f32945b2238c2bc54955bf81e9d0d8fa1
Hyunghwan Shin <hwan.shin@gmail.com>
2023-06-15 06:58:04 +0900

    update

--- kc/k2.py
+++ kc/k2.py
@@ -32,6 +32,9 @@ SUBNET_MASK = '255.255.255.0'
 COMM_PORT = '/dev/ttyUSB0'
 GATEWAY = '192.168.0.1'
 
+WIDGET_DX = 30
+CENTER_WIDGET_DX = 300
+
 ctk.set_appearance_mode(
     "Light")  #"System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme(
@@ -95,13 +98,13 @@ class App(ctk.CTk):
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        self.sidebar_frame = ctk.CTkFrame(self, width=160, corner_radius=0)  #,
-        self.sidebar_frame.grid(row=0, column=0, rowspan=50, sticky="nsew")
+        self.sidebar_frame = ctk.CTkFrame(self, width=220,  corner_radius=0)  #,
+        self.sidebar_frame.grid(row=0, column=0, rowspan=50, sticky="nsw")
 
-        self.send_frame = ctk.CTkFrame( self, width=220, corner_radius=0, fg_color='transparent')
+        self.send_frame = ctk.CTkFrame( self,  corner_radius=0, fg_color='transparent')
         self.send_frame.grid(row=0, column=1, rowspan=3, sticky="nsew")
 
-        self.receive_frame = ctk.CTkFrame(self, width=220, corner_radius=0, fg_color='transparent')
+        self.receive_frame = ctk.CTkFrame(self, width=420, corner_radius=0, fg_color='transparent')
         self.receive_frame.grid(row=0, column=2, rowspan=3, sticky="nsew")
 
         #self.sidebar_frame.grid_rowconfigure(2, weight=1)
@@ -112,41 +115,41 @@ class App(ctk.CTk):
         self.option_button = ctk.CTkButton(self.sidebar_frame, fg_color='#CC6600', hover_color='#AA4400',
                                          command=self.read_option_event)
         self.option_button.configure(text="설정 읽어오기")
-        self.option_button.grid(row=0, column=0, padx=10, pady=(40, 1), sticky="nw")
+        self.option_button.grid(row=0, column=0, padx=10+WIDGET_DX, pady=(40, 1), sticky="nw")
 
         self.label_serial = ctk.CTkLabel(self.sidebar_frame, text="시리얼 통신 포트")
-        self.label_serial.grid(row=1, column=0, padx=10, pady=(20, 1), sticky="nw")
+        self.label_serial.grid(row=1, column=0, padx=10+WIDGET_DX, pady=(20, 1), sticky="nw")
 
         self.entry_serial_port = ctk.CTkEntry(self.sidebar_frame)#, placeholder_text="COM2")
-        self.entry_serial_port.grid(row=2, column=0, padx=(10,20), pady=1, sticky="nw")
+        self.entry_serial_port.grid(row=2, column=0, padx=(10+WIDGET_DX,20), pady=1, sticky="nw")
 
         self.label_network = ctk.CTkLabel(self.sidebar_frame, text="단말 네트워크 설정")
-        self.label_network.grid(row=9, column=0, padx=10, pady=(30,1), sticky="nw")
+        self.label_network.grid(row=9, column=0, padx=10+WIDGET_DX, pady=(30,1), sticky="nw")
 
         self.label_ip = ctk.CTkLabel(self.sidebar_frame, text="IP")
-        self.label_ip.grid(row=10, column=0, padx=15, pady=(1,0), sticky="nw")
+        self.label_ip.grid(row=10, column=0, padx=15+WIDGET_DX, pady=(1,0), sticky="nw")
         self.switch_var = ctk.StringVar(value="NO-DHCP")
         self.dhcp = ctk.CTkSwitch(self.sidebar_frame, text="DHCP", command=self.dhcp_event,
                                    variable=self.switch_var, onvalue="DHCP", offvalue="NO-DHCP")
-        self.dhcp.grid(row=10, column=0, padx=80, pady=(1,0), sticky="nw")
+        self.dhcp.grid(row=10, column=0, padx=80+WIDGET_DX, pady=(1,0), sticky="nw")
 
         self.entry_ip = ctk.CTkEntry(self.sidebar_frame)
-        self.entry_ip.grid(row=11, column=0, padx=10, pady=0, sticky="nw")
+        self.entry_ip.grid(row=11, column=0, padx=10+WIDGET_DX, pady=0, sticky="nw")
 
         self.label_gateway = ctk.CTkLabel(self.sidebar_frame, text="gateway")
-        self.label_gateway.grid(row=12, column=0, padx=15, pady=(1,0), sticky="nw")
+        self.label_gateway.grid(row=12, column=0, padx=15+WIDGET_DX, pady=(1,0), sticky="nw")
         self.entry_gateway = ctk.CTkEntry(self.sidebar_frame)
-        self.entry_gateway.grid(row=13, column=0, padx=10, pady=0, sticky="nw")
+        self.entry_gateway.grid(row=13, column=0, padx=10+WIDGET_DX, pady=0, sticky="nw")
 
         self.label_subnet = ctk.CTkLabel(self.sidebar_frame, text="subnet mask")
-        self.label_subnet.grid(row=14, column=0, padx=15, pady=(1,0), sticky="nw")
+        self.label_subnet.grid(row=14, column=0, padx=15+WIDGET_DX, pady=(1,0), sticky="nw")
         self.entry_subnet = ctk.CTkEntry(self.sidebar_frame )
-        self.entry_subnet.grid(row=15, column=0, padx=10, pady=0, sticky="nw")
+        self.entry_subnet.grid(row=15, column=0, padx=10+WIDGET_DX, pady=0, sticky="nw")
 
         self.label_port = ctk.CTkLabel(self.sidebar_frame, text="port")
-        self.label_port.grid(row=16, column=0, padx=15, pady=(1,0), sticky="nw")
+        self.label_port.grid(row=16, column=0, padx=15+WIDGET_DX, pady=(1,0), sticky="nw")
         self.entry_port = ctk.CTkEntry(self.sidebar_frame, placeholder_text=f"{PORT_DEC}")
-        self.entry_port.grid(row=17, column=0, padx=10, pady=0, sticky="nw")
+        self.entry_port.grid(row=17, column=0, padx=10+WIDGET_DX, pady=0, sticky="nw")
 
         #self.label_dec_status = ctk.CTkLabel(self.sidebar_frame, fg_color='grey', text="복호화 서버 중지됨")
         #self.label_dec_status.grid(row=17, column=0, padx=10, pady=(10,1), sticky="nw")
@@ -154,24 +157,24 @@ class App(ctk.CTk):
         self.sv_key = ctk.StringVar()
         self.sv_key.trace("w", lambda name, index, mode, sv=self.sv_key: limit_key(sv))
         self.label_key = ctk.CTkLabel(self.sidebar_frame, text="암호화 키(16글자 이내)")
-        self.label_key.grid(row=30, column=0, padx=10, pady=(30,1), sticky="nw")
+        self.label_key.grid(row=30, column=0, padx=10+WIDGET_DX, pady=(30,1), sticky="nw")
         self.entry_key = ctk.CTkEntry(self.sidebar_frame, textvariable=self.sv_key,
                                       fg_color="#00c177", text_color='white', placeholder_text="12345678")
-        self.entry_key.grid(row=31, column=0, padx=10, pady=1, sticky="nw")
+        self.entry_key.grid(row=31, column=0, padx=10+WIDGET_DX, pady=1, sticky="nw")
 
         self.label_cipher = ctk.CTkLabel(self.sidebar_frame, text="암호화 방식:")  #,
-        self.label_cipher.grid(row=32, column=0, padx=10, pady=1, sticky="nw")
+        self.label_cipher.grid(row=32, column=0, padx=10+WIDGET_DX, pady=1, sticky="nw")
 
         self.cipher_optionemenu = ctk.CTkOptionMenu(
             self.sidebar_frame,
             values=["AES"],
             command=self.change_cipher_event)
-        self.cipher_optionemenu.grid(row=33, column=0, padx=10, pady=(0, 10), sticky="nw")
+        self.cipher_optionemenu.grid(row=33, column=0, padx=10+WIDGET_DX, pady=(0, 10), sticky="nw")
 
         self.option_button = ctk.CTkButton(self.sidebar_frame, fg_color='#CC6600', hover_color='#AA4400',
                                          command=self.apply_option_event)
         self.option_button.configure(text="설정 저장")
-        self.option_button.grid(row=34, column=0, padx=10, pady=(30, 1), sticky="nw")
+        self.option_button.grid(row=34, column=0, padx=10+WIDGET_DX, pady=(30, 1), sticky="nw")
 
         #=================================================================================
         # Send Frame
@@ -184,7 +187,7 @@ class App(ctk.CTk):
         self.label_plaintext = ctk.CTkLabel(self.send_frame, text="평문(16글자 이내):")
         self.label_plaintext.grid(row=1, column=0, padx=20, pady=(40, 0), sticky="nw")
         self.entry_plaintext = ctk.CTkEntry(self.send_frame, textvariable=self.sv_plaintext, 
-                                            width=400, placeholder_text="16자 이내의 암호화할 데이터를 입력하세요")
+                                            width=CENTER_WIDGET_DX, placeholder_text="16자 이내의 암호화할 데이터를 입력하세요")
         self.entry_plaintext.grid(row=1, column=0, padx=150, pady=(40,0), sticky="nwe")
 
         self.enc_button = ctk.CTkButton(self.send_frame, width=200, command=self.enc_button_event)
@@ -193,7 +196,7 @@ class App(ctk.CTk):
 
         self.label_ciphertext = ctk.CTkLabel(self.send_frame, text="암호문:")
         self.label_ciphertext.grid(row=3, column=0, padx=20, pady=(30, 0), sticky="nw")
-        self.ciphertextbox = ctk.CTkTextbox(self.send_frame, width=500, border_width = 2, border_color='gray', fg_color='light gray')
+        self.ciphertextbox = ctk.CTkTextbox(self.send_frame, width=CENTER_WIDGET_DX, border_width = 2, border_color='gray', fg_color='light gray')
         self.ciphertextbox.grid(row=3, column=0, padx=70, pady=(30,0), sticky="nwe")
         self.ciphertextbox.configure(state='disabled')
 
@@ -203,7 +206,7 @@ class App(ctk.CTk):
 
         self.label_dectext = ctk.CTkLabel(self.send_frame, text="복호문:")
         self.label_dectext.grid(row=5, column=0, padx=20, pady=(30, 0), sticky="nw")
-        self.entry_dectext = ctk.CTkEntry(self.send_frame, width=400, border_width = 2, border_color='gray', fg_color='light gray')
+        self.entry_dectext = ctk.CTkEntry(self.send_frame, width=CENTER_WIDGET_DX, border_width = 2, border_color='gray', fg_color='light gray')
         self.entry_dectext.grid(row=5, column=0, padx=70, pady=(30,0), sticky="nwe")
         self.entry_dectext.configure(state='disabled')
 
@@ -317,7 +320,7 @@ class App(ctk.CTk):
             self.comm_port = self.entry_serial_port.get()
             device = serial.Serial(port=self.comm_port, baudrate=BAUD_RATE, bytesize=8, parity='N', stopbits=1, timeout=SERIAL1_TIMEOUT, write_timeout=SERIAL1_TIMEOUT) 
         except serial.serialutil.SerialException as e:
-            CTkMessagebox(title="Info", message=f"시리얼 연결 오류: COM 포트({_app.comm_port})를 확인하세요.")
+            CTkMessagebox(title="Info", message=f"시리얼 연결 오류: COM 포트({self.comm_port})를 확인하세요.")
             logging.debug('시리얼 예외 발생: ', e)
         else:
             #while not current_thread().stopped() or not app.stop_thread:
